a70d3e42cbc596602c2393eadcc82e085b5a606f
paigeswarthout <pas265@miami.edu>
2025-01-28 16:24:26 -0500

    Lab 2

--- static-node/static.js
+++ static-node/static.js
@@ -10,20 +10,55 @@ const port = 3000;
 const someName = (name)=>{
     console.log("Your name is " + name);
 }
+// other way to write a function
+function someOtherNam(name){
+  console.log(name);
+}
+
 someName("Paige Swarthout");
+//create a function to read files and display them
+const displayPage = (path, r, status = 200)=>{
+
+r.setHeader('Content-Type', 'text/html');
+
+fs.readFile(path,(error,content)=>{
+    //We need to handle errors first
+    if(error){
+      r.statusCode = 500;
+      r.end("500 - error");
+    }else{
+    r.statusCode = status;
+    //if there are no errors, we can output the content
+    r.end(content);
+    }
+})
+
+}
 
 const server = http.createServer((req, res) => {
 console.log(req.url)
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-fs.readFile('./public/home.html',(error,content)=>{
-    //We need to handle errors first
 
-    //if there are no errors, we can output the content
-    res.end(content);
-})
+switch(req.url){
+  case "":
+  case "/":
+    //respond to /
+    displayPage('./public/home.html', res);
+    break;
 
+    case "/about":
+    //respond to /
+    displayPage('./public/about.html', res);
+    break;
 
+    case "/contact":
+    //respond to /
+    displayPage('./public/contact.html', res);
+    break;
+
+    default:
+    displayPage('./public/404.html', res, 404);
+
+}
 });
 
 server.listen(port, hostname, () => {
